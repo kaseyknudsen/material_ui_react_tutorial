db5e2e18918234fdaf269a34c2858ab126f3487f
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-02-16 10:22:14 -0800

    text field

--- src/App.js
+++ src/App.js
@@ -6,11 +6,46 @@ import SendIcon from "@mui/icons-material/Send";
 import { ButtonGroup } from "@mui/material";
 import AudiotrackIcon from "@mui/icons-material/Audiotrack";
 import ElectricScooterIcon from "@mui/icons-material/ElectricScooter";
+import { Checkbox } from "@mui/material";
+import { useState } from "react";
+import FormControl from "@mui/material/FormControl";
+import { FormControlLabel } from "@mui/material";
+import { TextField } from "@mui/material";
+
+function CheckBoxExample() {
+  const [checked, setChecked] = useState(true);
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checked}
+            icon={<SendIcon />}
+            checkedIcon={<SaveAltIcon />}
+            onChange={(e) => setChecked(e.target.checked)}
+            inputProps={{ "aria-label": "secondary checkbox" }}
+          />
+        }
+        label="Testing Checkbox"
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TextField 
+        variant="outlined"
+        color="secondary"
+        // type="date"
+        // type="time"
+        type="email"
+        placeholder="kasey@kasey.com"
+       
+        />
+        <CheckBoxExample />
         <Button
           startIcon={<SaveAltIcon />}
           endIcon={<SendIcon />}
